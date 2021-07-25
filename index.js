22f8ebe79fd421567005d44b5a8d30ac74c31721
arjung5 <arjungargniec@gmail.com>
2021-07-25 21:22:21 +0530

    Set Up the view engine ejs

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port =8080;
 //use express router
 app.use('/',require('./routes'));
 
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,(err)=>{
     if(err) console.log(`Error occured at creating the server ${err}`)
     else console.log(`The server is runnning at port ${port}`);
