bfa03a31b642724379dd871cdf83a58a24d8f41e
arjung5 <arjungargniec@gmail.com>
2021-07-26 08:34:08 +0530

    Set up the layouts and partials for the project

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,8 @@
-module.exports.profile=(req,res)=>{return res.end('<h1>This is User_Controller</h1>')};
+module.exports.profile=(req,res)=>{
+    return res.render('user_profile',{
+        title:"User Profile Page"
+    })
+};
 module.exports.userfriend=(req,res)=>{return res.end('<h1>This is User_Controller and action is friendList</h1>')};
 
 
