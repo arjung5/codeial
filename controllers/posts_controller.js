28505f08da3d5f2140e1559ec2767a1d10ad0c36
arjung5 <arjungargniec@gmail.com>
2021-07-27 17:06:21 +0530

    added post page and added to db

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,35 @@
+const Post=require('../models/post');
+
 module.exports.posts=(req,res)=>{
-    return res.end('<p>This is the posts controller</p>');
+    return res.render('post',{
+        title:"This is Post Page"
+    });
 }
+
+
+module.exports.CreatePost=(req,res)=>{
+    if(req.isAuthenticated())
+    {
+        console.log(`This is the req is for the logged user that is:= ${req.user.id}`)
+        Post.create({
+            content:req.body.content,
+            user:req.user.id
+        },(err,data)=>{
+            if(err)
+            {
+                console.log(`This the error caught while creating post in db and the error is :- ${err}`);
+                return;
+            }
+            return res.redirect('/');
+        })
+    }
+    else
+    {
+        res.redirect('/users/sign-in');
+    }
+
+
+}
+
+
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,13 @@
 const express=require('express');
 const router=express.Router();
+const passport=require('passport');
+
 
 const postController=require('../controllers/posts_controller');
 
 
 router.get('/',postController.posts);
 
+router.post('/create',postController.CreatePost);
 
 module.exports=router;
